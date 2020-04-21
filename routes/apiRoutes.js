955246b3b389b4ba2a764bce7eeb9f05036369a0
KantKontrol <nick.derissio.tech@gmail.com>
2020-04-21 11:31:52 -0400

    updated api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,33 @@
 const db = require("../models");
+const mongojs = require("mongojs");
 
 module.exports = function(server){
 
 
     server.get("/api/workouts", (req, res) => {
         db.Workout.find({}, data => {
+
+
+            res.json(data);
+        }); 
+    });
+
+    server.put("/api/workouts/:id", ({ body }, res) => {
+
+        db.Workout.create({
+            day: 
+        })
+
+    });
+
+
+    server.get("/excersize/:id", (req, res) => {
+
+        let id = req.params.id;
+
+        db.Workout.find({
+            __id: mongojs.ObjectId(id)
+        }, data => {
 
 
             res.json(data);
